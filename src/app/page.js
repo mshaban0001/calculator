93cfb385e8404c95ef3253b589c0c3d3ba9616ce
Muhammad Shaban <shaban@teknovation.io>
2023-12-11 11:31:35 +0500

    Functionality Done

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,57 @@
-import Image from 'next/image';
+'use client';
+
+import { useState } from 'react';
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleButtonClick = (value) => {
+    setInputValue((prevValue) => prevValue + value);
+  };
+
+  const handleCalculate = () => {
+    try {
+      setInputValue(eval(inputValue).toString());
+    } catch (error) {
+      setInputValue('Error');
+    }
+  };
+
+  const handleClear = () => {
+    setInputValue('');
+  };
+
   return (
     <main className='flex h-screen flex-col items-center justify-center '>
       <div className='bg-[#a592b9] w-full h-full py-16 px-6 flex flex-col gap-32 '>
         <div className='w-full h-14  '>
           <input
-            type='number'
+            type='text'
+            value={inputValue}
             className='w-full h-full rounded-2xl outline-none px-4'
+            readOnly
           />
         </div>
 
         <div class='bg-[#b4a3c5] px-10 py-8 rounded-xl grid grid-cols-4 gap-12'>
-          <button class='btn'>1</button>
+          {[1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, '/', 0, '.', '=', '*'].map(
+            (btnValue, index) => (
+              <button
+                key={index}
+                className='btn'
+                onClick={() =>
+                  btnValue === '='
+                    ? handleCalculate()
+                    : handleButtonClick(btnValue)
+                }>
+                {btnValue}
+              </button>
+            )
+          )}
+          <button className='btn' onClick={handleClear}>
+            C
+          </button>
+          {/* <button class='btn'>1</button>
           <button class='btn'>2</button>
           <button class='btn'>3</button>
           <button class='btn'>+</button>
@@ -30,7 +69,7 @@ export default function Home() {
           <button class='btn'>0</button>
           <button class='btn'>.</button>
           <button class='btn'>=</button>
-          <button class='btn'>*</button>
+          <button class='btn'>*</button> */}
         </div>
       </div>
     </main>
