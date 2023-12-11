ba1303b0f7a5c299244994f0daf67d05217a0d7f
Muhammad Shaban <shaban@teknovation.io>
2023-12-11 10:40:25 +0500

    Calculator App Setup

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className='flex h-screen flex-col items-center justify-center '>
+      <div className='bg-red-500 w-full h-full'></div>
+    </main>
+  );
+}
