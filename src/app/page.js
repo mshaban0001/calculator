7e3657dadf9e8233861fabb00fc44a0a25da5349
Muhammad Shaban <shaban@teknovation.io>
2023-12-11 10:57:45 +0500

    Inputfield Done

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,14 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className='flex h-screen flex-col items-center justify-center '>
-      <div className='bg-red-500 w-full h-full'></div>
+      <div className='bg-[#a6b992] w-full h-full py-16'>
+        <div className='w-full h-14 px-6 '>
+          <input
+            type='number'
+            className='w-full h-full rounded-2xl outline-none px-4'
+          />
+        </div>
+      </div>
     </main>
   );
 }
